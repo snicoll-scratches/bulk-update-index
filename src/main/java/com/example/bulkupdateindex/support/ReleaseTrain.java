8a0bfb40e19974040709671412e69e044e21ac3a
Stephane Nicoll <snicoll@pivotal.io>
2017-10-16 13:57:09 +0200

    Add release train support

---
+++ src/main/java/com/example/bulkupdateindex/support/ReleaseTrain.java
@@ -0,0 +1,41 @@
+package com.example.bulkupdateindex.support;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Stephane Nicoll
+ */
+public class ReleaseTrain {
+
+	private final String name;
+	private final String qualifier;
+
+	private static final Pattern RELEASE_TRAIN_REGEX =
+			Pattern.compile("([A-Za-z]*)(_|-|.)([A-Za-z0-9_-]*)");
+
+
+	public ReleaseTrain(String name, String qualifier) {
+		this.name = name;
+		this.qualifier = qualifier;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getQualifier() {
+		return this.qualifier;
+	}
+
+	public static ReleaseTrain safeParse(String text) {
+		Matcher matcher = RELEASE_TRAIN_REGEX.matcher(text.trim());
+		if (!matcher.matches()) {
+			return null;
+		}
+		String name = matcher.group(1);
+		String qualifier = matcher.group(3);
+		return new ReleaseTrain(name, qualifier);
+	}
+}
---
+++ src/test/java/com/example/bulkupdateindex/support/ReleaseTrainTests.java
@@ -0,0 +1,43 @@
+package com.example.bulkupdateindex.support;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author Stephane Nicoll
+ */
+public class ReleaseTrainTests {
+
+	@Test
+	public void parseSpringDataReleaseTrain() {
+		ReleaseTrain releaseTrain = ReleaseTrain.safeParse("Ingalls-SR5-1");
+		assertThat(releaseTrain).isNotNull();
+		assertThat(releaseTrain.getName()).isEqualTo("Ingalls");
+		assertThat(releaseTrain.getQualifier()).isEqualTo("SR5-1");
+	}
+
+	@Test
+	public void parseSpringCloudReleaseTrain() {
+		ReleaseTrain releaseTrain = ReleaseTrain.safeParse("Dalston.SR4");
+		assertThat(releaseTrain).isNotNull();
+		assertThat(releaseTrain.getName()).isEqualTo("Dalston");
+		assertThat(releaseTrain.getQualifier()).isEqualTo("SR4");
+	}
+
+	@Test
+	public void parseReactorReleaseTrain() {
+		ReleaseTrain releaseTrain = ReleaseTrain.safeParse("Bismuth-RELEASE");
+		assertThat(releaseTrain).isNotNull();
+		assertThat(releaseTrain.getName()).isEqualTo("Bismuth");
+		assertThat(releaseTrain.getQualifier()).isEqualTo("RELEASE");
+	}
+
+	@Test
+	public void shouldNotParseReleaseVersion() {
+		ReleaseTrain releaseTrain = ReleaseTrain.safeParse("1.0-rc1");
+		assertThat(releaseTrain).isNull();
+	}
+	
+}
