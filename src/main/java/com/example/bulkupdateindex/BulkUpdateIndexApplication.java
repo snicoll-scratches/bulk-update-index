685ea545c3af0e3652eab11f2dfcde60851ca7eb
Stephane Nicoll <snicoll@pivotal.io>
2018-12-19 14:15:50 +0100

    Polish

--- src/main/java/com/example/bulkupdateindex/BulkUpdateIndexApplication.java
+++ src/main/java/com/example/bulkupdateindex/BulkUpdateIndexApplication.java
@@ -1,11 +1,7 @@
 package com.example.bulkupdateindex;
 
-import java.io.IOException;
-
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BulkUpdateIndexApplication {
@@ -14,17 +10,4 @@ public class BulkUpdateIndexApplication {
 		SpringApplication.run(BulkUpdateIndexApplication.class, args);
 	}
 
-	@Bean
-	public ApplicationRunner applicationRunner(DependenciesIdUpdater updater) throws IOException {
-		return (args) -> {
-			updater.indexDependencies("initializr-2017");
-		};
-	}
-
-	@Bean
-	public ApplicationRunner moduleVersionsUpdate(ModuleVersionUpdater updater) throws IOException {
-		return (args) -> {
-			//updater.indexVersions();
-		};
-	}
 }
