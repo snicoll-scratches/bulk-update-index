46122da1a397b9cefd887f74043439da033bef25
Stephane Nicoll <snicoll@pivotal.io>
2018-12-21 14:47:09 +0100

    Update to use VersionReference

--- src/main/java/com/example/bulkupdateindex/download/DownloadCountAggregation.java
+++ src/main/java/com/example/bulkupdateindex/download/DownloadCountAggregation.java
@@ -16,14 +16,10 @@
 
 package com.example.bulkupdateindex.download;
 
-import java.net.URLDecoder;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import com.example.bulkupdateindex.support.ReleaseTrain;
-import com.example.bulkupdateindex.support.Version;
+import com.example.bulkupdateindex.support.VersionReference;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,76 +55,29 @@ class DownloadCountAggregation {
 	boolean handle(JsonObject source, JsonObject stat) {
 		long count = stat.get("count").getAsLong();
 		this.totalCount += count;
-		String version = cleanVersion(stat.get("version").getAsString());
-
-		Version standardVersion = Version.safeParse(version);
-		if (standardVersion != null && standardVersion.getMajor() != null) {
-			handleVersion(standardVersion, count);
-			return true;
+		String versionText = stat.get("version").getAsString();
+		VersionReference versionReference = VersionReference.parse(versionText);
+		if (versionReference != null) {
+			handle(versionReference, count);
 		}
-
-		ReleaseTrain releaseTrain = ReleaseTrain.safeParse(version);
-		if (releaseTrain != null) {
-			handle(null, releaseTrain.getName(), count);
-			return true;
+		else {
+			String moduleId = source.get("groupId").getAsString() + " - "
+					+ source.get("artifactId").getAsString();
+			logger.warn(String.format("Skipping %s [count=%s, version='%s']", moduleId,
+					count, versionText));
 		}
-
-		Version nonStandardVersion = safeNonStandardVersionParse(version);
-		if (nonStandardVersion != null) {
-			handleVersion(nonStandardVersion, count);
-			return true;
-		}
-
-		String moduleId = source.get("groupId").getAsString() + " - "
-				+ source.get("artifactId").getAsString();
-		logger.warn(String.format("Skipping %s [count=%s, version='%s']", moduleId, count,
-				version));
-		return false;
+		return versionReference != null;
 	}
 
-	private String cleanVersion(String version) {
-		try {
-			String cleanVersion = URLDecoder.decode(version, "UTF-8");
-			int i = cleanVersion.lastIndexOf("?");
-			return (i != -1) ? cleanVersion.substring(i + 1) : cleanVersion;
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-			return version;
-		}
-	}
-
-	private void handleVersion(Version standardVersion, long count) {
-		String major = String.format("%s", standardVersion.getMajor());
-		String minor = (standardVersion.getMinor() != null) ? String.format("%s.%s",
-				standardVersion.getMajor(), standardVersion.getMinor()) : null;
-		handle(major, minor, count);
-	}
-
-	private void handle(String major, String minor, Long count) {
-		if (major != null) {
-			this.majorGenerations.compute(major,
+	private void handle(VersionReference versionReference, Long count) {
+		if (versionReference.getMajor() != null) {
+			this.majorGenerations.compute(versionReference.getMajor(),
 					(key, v) -> (v != null) ? v + count : count);
 		}
-		if (minor != null) {
-			this.minorGenerations.compute(minor,
+		if (versionReference.getMinor() != null) {
+			this.minorGenerations.compute(versionReference.getMinor(),
 					(key, v) -> (v != null) ? v + count : count);
 		}
-	}
-
-	private static final Pattern NON_STANDARD_VERSION_REGEX = Pattern
-			.compile("^(\\d+)\\.(\\d+|x)(?:[.|-]([^0-9]+)(\\d+)?)?$");
-
-	private Version safeNonStandardVersionParse(String text) {
-		Matcher matcher = NON_STANDARD_VERSION_REGEX.matcher(text.trim());
-		if (!matcher.matches()) {
-			return null;
-		}
-		Integer major = Integer.valueOf(matcher.group(1));
-		Integer minor = Integer.valueOf(matcher.group(2));
-		String qualifier = matcher.group(3);
-		return new Version(major, minor, null, new Version.Qualifier(qualifier));
-
 	}
 
 }
