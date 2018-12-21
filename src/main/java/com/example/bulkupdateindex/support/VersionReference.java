951786ac17b44eeb15d03fbf4ae1d0844c6a4dff
Stephane Nicoll <snicoll@pivotal.io>
2018-12-21 14:46:44 +0100

    Add VersionReference

---
+++ src/main/java/com/example/bulkupdateindex/support/VersionReference.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bulkupdateindex.support;
+
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * An object that gathers a version and its major and minor references.
+ *
+ * @author Stephane Nicoll
+ */
+public final class VersionReference {
+
+	private static final Pattern NON_STANDARD_VERSION_REGEX = Pattern
+			.compile("^(\\d+)\\.(\\d+|x)(?:[.|-]([^0-9]+)(\\d+)?)?$");
+
+	private final String id;
+
+	private final String major;
+
+	private final String minor;
+
+	private VersionReference(String id, String major, String minor) {
+		this.id = id;
+		this.major = major;
+		this.minor = minor;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getMajor() {
+		return this.major;
+	}
+
+	public String getMinor() {
+		return this.minor;
+	}
+
+	public static VersionReference of(String id, String major, String minor) {
+		return new VersionReference(id, major, minor);
+	}
+
+	public static VersionReference parse(String text) {
+		String versionText = cleanVersion(text);
+
+		Version standardVersion = Version.safeParse(versionText);
+		if (standardVersion != null && standardVersion.getMajor() != null) {
+			return fromVersion(versionText, standardVersion);
+		}
+
+		ReleaseTrain releaseTrain = ReleaseTrain.safeParse(text);
+		if (releaseTrain != null) {
+			return of(versionText, null, releaseTrain.getName());
+		}
+
+		Version nonStandardVersion = safeNonStandardVersionParse(versionText);
+		if (nonStandardVersion != null) {
+			return fromVersion(versionText, nonStandardVersion);
+		}
+		return null;
+	}
+
+	private static VersionReference fromVersion(String id, Version version) {
+		String major = String.format("%s", version.getMajor());
+		String minor = (version.getMinor() != null)
+				? String.format("%s.%s", version.getMajor(), version.getMinor()) : null;
+		return of(id, major, minor);
+	}
+
+	private static String cleanVersion(String version) {
+		try {
+			String cleanVersion = URLDecoder.decode(version, "UTF-8");
+			int i = cleanVersion.lastIndexOf("?");
+			return (i != -1) ? cleanVersion.substring(i + 1) : cleanVersion;
+		}
+		catch (Exception ex) {
+			return version;
+		}
+	}
+
+	private static Version safeNonStandardVersionParse(String text) {
+		Matcher matcher = NON_STANDARD_VERSION_REGEX.matcher(text.trim());
+		if (!matcher.matches()) {
+			return null;
+		}
+		Integer major = Integer.valueOf(matcher.group(1));
+		Integer minor = Integer.valueOf(matcher.group(2));
+		String qualifier = matcher.group(3);
+		return new Version(major, minor, null, new Version.Qualifier(qualifier));
+
+	}
+
+}
---
+++ src/test/java/com/example/bulkupdateindex/support/VersionReferenceTests.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bulkupdateindex.support;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link VersionReference}.
+ *
+ * @author Stephane Nicoll
+ */
+public class VersionReferenceTests {
+
+	@Test
+	public void parseSimpleVersion() {
+		testParser("1.2.3.RELEASE", "1", "1.2");
+	}
+
+	@Test
+	public void parseVersionWithDashedQualifier() {
+		testParser("2.0.0-M1", "2", "2.0");
+	}
+
+	@Test
+	public void parseNoQualifier() {
+		testParser("2.1.4", "2", "2.1");
+	}
+
+	@Test
+	public void parseWithQuestionMarkPrefix() {
+		testParser("?2.1.4", "2.1.4", "2", "2.1");
+	}
+
+	@Test
+	public void parseWithEncodedVersion() {
+		testParser("4%2E1%2E6%2ERELEASE", "4.1.6.RELEASE", "4", "4.1");
+	}
+
+	@Test
+	public void parseWithMajorMinorOnly() {
+		testParser("2.0", "2", "2.0");
+	}
+
+	@Test
+	public void parseReleaseTrain() {
+		testParser("Gosling-SR1", null, "Gosling");
+	}
+
+	@Test
+	public void parseVersionProperty() {
+		assertThat(VersionReference.parse("${spring.version}")).isNull();
+	}
+
+	private void testParser(String text, String major, String minor) {
+		testParser(text, text, major, minor);
+	}
+
+	private void testParser(String text, String id, String major, String minor) {
+		VersionReference reference = VersionReference.parse(text);
+		assertThat(reference).isNotNull();
+		assertThat(reference.getId()).isEqualTo(id);
+		assertThat(reference.getMajor()).isEqualTo(major);
+		assertThat(reference.getMinor()).isEqualTo(minor);
+	}
+
+}
